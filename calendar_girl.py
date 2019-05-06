3d3e7e64b8e122fedd82487a9a4dc0db8baddf60
Amanda Liu <aliu0131@gmail.com>
2019-05-05 22:12:05 -0400

    Add files via upload
    new command "addEvent" reads and writes input to a text file

--- calendar_girl.py
+++ calendar_girl.py
@@ -6,6 +6,7 @@ Created on Tue Aug  7 11:35:20 2018
 """
 import datetime
 from discord.ext.commands import Bot
+from discord.ext import commands
 
 
 BOT_PREFIX = ("!")
@@ -14,10 +15,20 @@ TOKEN = "NDc2MzY5MzI3MzkwNTg4OTI5.DksmTA.YMis-rZ-ax5CFcBp1Wm845S8HEs"
 client = Bot(command_prefix=BOT_PREFIX)
 
 @client.command()
-async def today(message):
+async def today(ctx):
     t = datetime.date.today()
-    await message.send("Today is " + t.strftime("%m/%d/%Y") + "\nIt is a " + t.strftime("%A")
+    await ctx.send("Today is " + t.strftime("%m/%d/%Y") + "\nIt is a " + t.strftime("%A")
                        + "\nHave a wonderful day ~")
+
+@client.command()
+async def addEvent(ctx, *event, date):
+    cal = open("calendar.txt", "a+")
+    cal.write(date + " " + event + "\n")
+    cal.close()
+    await ctx.send("Event added!")
+
+
+
 
 
 client.run(TOKEN)
